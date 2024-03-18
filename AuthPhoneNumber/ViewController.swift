2f5900e9981885bfa4f2cd1c319147a7b9ac3f50
varshaver12 <varshaver12@yandex.ru>
2024-03-18 19:52:10 +0300

    Initial Commit

---
+++ AuthPhoneNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthPhoneNumber
+//
+//  Created by Aleksey Khlestkin on 18.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
